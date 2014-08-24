2de1b865c028e253f71d1f4384d64f7f900676ec
luyi <xs0210456@gmail.com>
2014-08-24 21:47:03 +0800

    init folders

---
+++ config.default.js
@@ -0,0 +1,10 @@
+var config = {
+    db: 'mongodb://127.0.0.1/stone_book_dev',
+    db_name: 'stone_book_dev',
+
+    session_secret: 'stonebook_dev',
+    debug: true
+};
+
+module.exports = config;
+module.exports.config = config;
